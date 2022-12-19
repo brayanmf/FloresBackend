a9b3d1f579be0c3079c966bc63de8dade6a74ac4
b mflores <brayan.mflores@hotmail.com>
2022-12-18 21:08:48 -0500

    [update] cambios index.js

--- config/index.js
+++ config/index.js
@@ -1,30 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const {
   NODE_ENV,
   DB_CONNECTION_URI,
   DB_CONNECTION_URI_TEST,
   DB_CONNECTION_URI_PROD,
-} = process.env;
+} = process.env
 
 let connectionString =
-  NODE_ENV === "test" ? DB_CONNECTION_URI_TEST : DB_CONNECTION_URI;
-if (NODE_ENV === "production") connectionString = DB_CONNECTION_URI_PROD;
+  NODE_ENV === 'test' ? DB_CONNECTION_URI_TEST : DB_CONNECTION_URI
+if (NODE_ENV === 'production') connectionString = DB_CONNECTION_URI_PROD
 
 const connectDatabase = () => {
- console.log(connectionString,"bmf")
   mongoose
     .connect(connectionString)
     .then(() =>
-      console.log("Successfully connected to DB <{", connectionString, "}>")
+      console.log('Successfully connected to DB <{', connectionString, '}>'),
     )
     .catch((err) =>
       console.log(
         "Couldn't connect to DB <{",
         connectionString,
-        "}>. Error: ",
-        err
-      )
-    );
-};
-module.exports = connectDatabase;
+        '}>. Error: ',
+        err,
+      ),
+    )
+}
+module.exports = connectDatabase
